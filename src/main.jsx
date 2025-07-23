f75849f2db0472feae1cc0d2517ffaa9bc9b0133
abesanets <abesanets@gmail.com>
2025-07-23 19:22:32 +0300

    adaptive width

--- src/main.jsx
+++ src/main.jsx
@@ -10,3 +10,14 @@ root.render(
     <Resume />
   </React.StrictMode>
 );
+
+function autoScale(baseWidth = 594) {
+const scaleWrapper = document.getElementById('root');
+  const currentWidth = window.innerWidth;
+  const scale = currentWidth / baseWidth;
+  scaleWrapper.style.transform = `scale(${scale})`;
+}
+
+// Вызываем при загрузке и при изменении размера окна
+window.addEventListener('load', () => autoScale());
+window.addEventListener('resize', () => autoScale());
