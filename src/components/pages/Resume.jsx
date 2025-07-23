c717d67cd04057ab303a702aadc4e5dec6b23051
abesanets <abesanets@gmail.com>
2025-07-23 17:12:58 +0300

    few fixes

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -4,15 +4,7 @@ import ResumeTemplate from '../templates/ResumeTemplate';
 
 const Resume = () => {
   return (
-    <div className="resume-container">
-      <div className="browser-toolbar">
-        <div className="controls">
-          <div className="control-btn close-btn"></div>
-          <div className="control-btn maximise-btn"></div>
-          <div className="control-btn minimise-btn"></div>
-        </div>
-      </div>
-      
+    <div className="resume-container">      
       <ResumeTemplate />
     </div>
   );
