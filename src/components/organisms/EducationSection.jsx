10ba36cc4c953c62d5135c97888c5fc49ff09236
abesanets <abesanets@gmail.com>
2025-07-24 11:01:51 +0300

    test fix

--- src/components/organisms/EducationSection.jsx
+++ src/components/organisms/EducationSection.jsx
@@ -6,7 +6,7 @@ const EducationSection = () => {
     {
       institution: "Memorisely",
       course: "Build a design system",
-      image: "/photos/education/memorizely.png",
+      image: "/public/photos/education/memorizely.png",
       date: "Oct 2021"
     },
     {
